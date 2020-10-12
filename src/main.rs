46c1e5d54854b62aeb398e5a9a941ac813ae5ab5
최재용 <jayong93@gmail.com>
2020-10-12 21:56:03 +0900

    refactor the code

--- src/main.rs
+++ src/main.rs
@@ -28,8 +28,11 @@ fn main() -> Result<()> {
     let commands: Vec<WebCommand> = from_str(&file_contents)?;
 
     let mut child = Command::new(&option.geckodriver_path).spawn()?;
-    let result = if let Ok(client) = runtime.block_on(Client::new("http://localhost:4444")) {
-        run_commands(client, commands, runtime)
+    let result = if let Ok(mut client) = runtime.block_on(Client::new("http://localhost:4444")) {
+        for command in &commands {
+            run_commands(&mut client, command, &mut runtime)?;
+        }
+        Ok(())
     } else {
         Ok(())
     };
@@ -39,113 +42,111 @@ fn main() -> Result<()> {
 }
 
 fn run_commands(
-    mut client: Client,
-    commands: Vec<WebCommand>,
-    mut runtime: tokio::runtime::Runtime,
+    client: &mut Client,
+    command: &WebCommand,
+    runtime: &mut tokio::runtime::Runtime,
 ) -> Result<()> {
     use fantoccini::{Element, Locator};
     use std::iter::from_fn;
-    for command in commands {
-        let mut c = Some(&command);
-        let mut it = from_fn(move || {
-            if let Some(com) = c {
-                match com.command_type {
-                    CommandType::Recursive(ref new_c) => {
-                        c = Some(new_c.as_ref());
-                    }
-                    _ => {
-                        c = None;
-                    }
+    let mut c = Some(command);
+    let mut it = from_fn(move || {
+        if let Some(com) = c {
+            match com.command_type {
+                CommandType::Recursive(ref new_c) => {
+                    c = Some(new_c.as_ref());
                 }
-                return Some(com);
-            } else {
-                return None;
+                _ => {
+                    c = None;
+                }
             }
-        });
+            return Some(com);
+        } else {
+            return None;
+        }
+    });
 
-        it.try_fold(
-            None,
-            |mut elem: Option<Element>, command: &WebCommand| -> Result<Option<Element>> {
-                let get_selector = || -> Result<&String> {
-                    command
-                        .selector
-                        .as_ref()
-                        .ok_or_else(|| anyhow!("A command needs a selector string"))
-                };
-                let get_next_locator = || -> Result<Locator> { Ok(Locator::Css(get_selector()?)) };
+    it.try_fold(
+        None,
+        |mut elem: Option<Element>, command: &WebCommand| -> Result<Option<Element>> {
+            let get_selector = || -> Result<&String> {
+                command
+                    .selector
+                    .as_ref()
+                    .ok_or_else(|| anyhow!("A command needs a selector string"))
+            };
+            let get_next_locator = || -> Result<Locator> { Ok(Locator::Css(get_selector()?)) };
 
-                match &command.command_type {
-                    CommandType::GoTo(url) => {
-                        runtime.block_on(client.goto(url))?;
+            match &command.command_type {
+                CommandType::GoTo(url) => {
+                    runtime.block_on(client.goto(url))?;
+                    Ok(elem)
+                }
+                CommandType::ChangeWindowSize { width, height } => {
+                    runtime.block_on(client.set_window_size(*width, *height))?;
+                    Ok(elem)
+                }
+                CommandType::ScrollIntoView => {
+                    let script = format!(
+                        "document.querySelector(\"{}\").scrollIntoView();",
+                        get_selector()?
+                    );
+                    runtime.block_on(client.execute(&script, vec![]))?;
+                    Ok(elem)
+                }
+                CommandType::WaitForSeconds(sec) => {
+                    std::thread::sleep(std::time::Duration::from_secs_f64(*sec));
+                    Ok(elem)
+                }
+                CommandType::ChangeWindow(i) => {
+                    let windows = runtime.block_on(client.windows())?;
+                    if let Some(window) = windows.get(*i).cloned() {
+                        runtime.block_on(client.switch_to_window(window))?;
                         Ok(elem)
-                    }
-                    CommandType::ChangeWindowSize { width, height } => {
-                        runtime.block_on(client.set_window_size(*width, *height))?;
-                        Ok(elem)
-                    }
-                    CommandType::ScrollIntoView => {
-                        let script = format!(
-                            "document.querySelector(\"{}\").scrollIntoView();",
-                            get_selector()?
-                        );
-                        runtime.block_on(client.execute(&script, vec![]))?;
-                        Ok(elem)
-                    }
-                    CommandType::WaitForSeconds(sec) => {
-                        std::thread::sleep(std::time::Duration::from_secs_f64(*sec));
-                        Ok(elem)
-                    }
-                    CommandType::ChangeWindow(i) => {
-                        let windows = runtime.block_on(client.windows())?;
-                        if let Some(window) = windows.get(*i).cloned() {
-                            runtime.block_on(client.switch_to_window(window))?;
-                            Ok(elem)
-                        } else {
-                            bail!("Couldn't find the window")
-                        }
-                    }
-                    CommandType::LeaveFrame => {
-                        client = runtime.block_on(client.clone().enter_parent_frame())?;
-                        Ok(elem)
-                    }
-                    CommandType::PrintSource => {
-                        eprintln!("{}", runtime.block_on(client.source())?);
-                        Ok(elem)
-                    }
-                    CommandType::Wait => {
-                        let locator = get_next_locator()?;
-                        elem = Some(runtime.block_on(client.wait_for_find(locator))?);
-                        Ok(elem)
-                    }
-                    _ => {
-                        let locator = get_next_locator()?;
-                        let mut new_elem: Element = if let Some(mut e) = elem {
-                            runtime.block_on(e.find(locator))?
-                        } else {
-                            runtime.block_on(client.find(locator))?
-                        };
-
-                        elem = Some(new_elem.clone());
-
-                        match &command.command_type {
-                            CommandType::EnterFrame => {
-                                client = runtime.block_on(new_elem.enter_frame())?;
-                            }
-                            CommandType::Click => {
-                                client = runtime.block_on(new_elem.click())?;
-                            }
-                            CommandType::Input(s) => {
-                                runtime.block_on(new_elem.send_keys(s))?;
-                            }
-                            CommandType::Recursive(_) => {}
-                            _ => unreachable!(),
-                        }
-
-                        Ok(elem)
+                    } else {
+                        bail!("Couldn't find the window")
                     }
                 }
-            },
-        )?;
-    }
+                CommandType::LeaveFrame => {
+                    *client = runtime.block_on(client.clone().enter_parent_frame())?;
+                    Ok(elem)
+                }
+                CommandType::PrintSource => {
+                    eprintln!("{}", runtime.block_on(client.source())?);
+                    Ok(elem)
+                }
+                CommandType::Wait => {
+                    let locator = get_next_locator()?;
+                    elem = Some(runtime.block_on(client.wait_for_find(locator))?);
+                    Ok(elem)
+                }
+                _ => {
+                    let locator = get_next_locator()?;
+                    let mut new_elem: Element = if let Some(mut e) = elem {
+                        runtime.block_on(e.find(locator))?
+                    } else {
+                        runtime.block_on(client.find(locator))?
+                    };
+
+                    elem = Some(new_elem.clone());
+
+                    match &command.command_type {
+                        CommandType::EnterFrame => {
+                            *client = runtime.block_on(new_elem.enter_frame())?;
+                        }
+                        CommandType::Click => {
+                            *client = runtime.block_on(new_elem.click())?;
+                        }
+                        CommandType::Input(s) => {
+                            runtime.block_on(new_elem.send_keys(s))?;
+                        }
+                        CommandType::Recursive(_) => {}
+                        _ => unreachable!(),
+                    }
+
+                    Ok(elem)
+                }
+            }
+        },
+    )?;
     Ok(())
 }
