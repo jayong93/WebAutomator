b04240fe445eeb5d8009218ce353416ff9f06623
최재용 <jayong93@gmail.com>
2020-10-23 13:31:44 +0900

    allow to use WaitForSeconds with selector

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use anyhow::{anyhow, bail, Result};
 use fantoccini::Client;
 use fantoccini::{Element, Locator};
+use futures::prelude::*;
 use std::fs::OpenOptions;
 use std::path::PathBuf;
+use std::time::Duration;
 use structopt::StructOpt;
 use tokio;
 use yaml2commands::{serde_yaml::from_str, CommandType, WebCommand};
@@ -19,14 +21,11 @@ struct CmdOption {
     geckodriver_path: String,
 }
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     use std::io::Read;
     use std::process::Command;
     let option = CmdOption::from_args();
-    let mut runtime = tokio::runtime::Builder::new()
-        .basic_scheduler()
-        .enable_all()
-        .build()?;
 
     let mut input_file = OpenOptions::new().read(true).open(option.input_file)?;
     let mut file_contents = String::new();
@@ -34,27 +33,26 @@ fn main() -> Result<()> {
     let commands: Vec<WebCommand> = from_str(&file_contents)?;
 
     let mut child = Command::new(&option.geckodriver_path).spawn()?;
-    let result = if let Ok(mut client) = runtime.block_on(Client::new("http://localhost:4444")) {
-        commands
-            .iter()
-            .try_for_each(|command| run_command(&mut client, command, &mut runtime))
-    } else {
-        Ok(())
-    };
+    let mut client = Client::new("http://localhost:4444").await?;
+    for command in commands {
+        if let Err(e) = run_command(&mut client, &command).await {
+            eprintln!("Error has occured: {}", e);
+            break;
+        }
+    }
     child.kill()?;
 
-    result
+    Ok(())
 }
 
-fn run_command(
-    client: &mut Client,
-    command: &WebCommand,
-    runtime: &mut tokio::runtime::Runtime,
-) -> Result<()> {
+fn run_command<'c>(
+    client: &'c mut Client,
+    command: &'c WebCommand,
+) -> future::BoxFuture<'c, Result<()>> {
     use std::iter::from_fn;
     let mut c = Some(command);
     // Make Recursive commands to iterator
-    let mut it = from_fn(move || {
+    let it = from_fn(move || {
         if let Some(com) = c {
             match com.command_type {
                 CommandType::Recursive(ref new_c) => {
@@ -70,20 +68,24 @@ fn run_command(
         }
     });
 
-    it.try_fold(
-        None,
-        |elem: Option<Element>, command: &WebCommand| -> Result<Option<Element>> {
-            do_command_detail(elem, command, client, runtime)
-        },
-    )?;
-    Ok(())
+    async move {
+        stream::iter(it)
+            .map(Result::Ok)
+            .try_fold((None, client), |(elem, client), command| async move {
+                do_command_detail(elem, command, client)
+                    .await
+                    .map(|e| (e, client))
+            })
+            .await?;
+        Ok(())
+    }
+    .boxed()
 }
 
-fn do_command_detail(
+async fn do_command_detail(
     elem: Option<Element>,
     command: &WebCommand,
     client: &mut Client,
-    runtime: &mut tokio::runtime::Runtime,
 ) -> Result<Option<Element>> {
     use fantoccini::error::CmdError;
     use webdriver::error::ErrorStatus;
@@ -99,19 +101,25 @@ fn do_command_detail(
     match &command.command_type {
         // Command types which don't need a element
         CommandType::GoTo(url) => {
-            runtime.block_on(client.goto(url))?;
+            client.goto(url).await?;
             Ok(elem)
         }
         CommandType::Loop(commands) => {
             loop {
-                let result = commands
-                    .into_iter()
-                    .try_for_each(|command| run_command(client, command, runtime));
+                let mut result = Ok(());
+                for command in commands {
+                    result = run_command(client, command).await;
+                    if result.is_err() {
+                        break;
+                    }
+                }
                 match result {
                     Ok(_) => break,
                     Err(e) => {
                         if let Some(CmdError::Standard(e)) = e.downcast_ref::<CmdError>() {
-                            if let ErrorStatus::NoSuchWindow | ErrorStatus::InvalidSessionId = e.error {
+                            if let ErrorStatus::NoSuchWindow | ErrorStatus::InvalidSessionId =
+                                e.error
+                            {
                                 bail!("A loop has failed: {}", e)
                             }
                             eprintln!("Failed to finish a loop: {}; will retry...", e);
@@ -124,7 +132,7 @@ fn do_command_detail(
             Ok(elem)
         }
         CommandType::ChangeWindowSize { width, height } => {
-            runtime.block_on(client.set_window_size(*width, *height))?;
+            client.set_window_size(*width, *height).await?;
             Ok(elem)
         }
         CommandType::ScrollIntoView => {
@@ -132,83 +140,88 @@ fn do_command_detail(
                 r#"document.querySelector("{}").scrollIntoView();"#,
                 get_selector()?
             );
-            runtime.block_on(client.execute(&script, vec![]))?;
+            client.execute(&script, vec![]).await?;
             Ok(elem)
         }
         CommandType::WaitForSeconds(sec) => {
-            std::thread::sleep(std::time::Duration::from_secs_f64(*sec));
+            if let Ok(locator) = get_next_locator() {
+                tokio::time::timeout(Duration::from_secs_f64(*sec), client.wait_for_find(locator))
+                    .await??;
+            } else {
+                std::thread::sleep(std::time::Duration::from_secs_f64(*sec));
+            }
             Ok(elem)
         }
         CommandType::ChangeWindow(i) => {
-            let windows = runtime.block_on(client.windows())?;
+            let windows = client.windows().await?;
             if let Some(window) = windows.get(*i).cloned() {
-                runtime.block_on(client.switch_to_window(window))?;
+                client.switch_to_window(window).await?;
                 Ok(elem)
             } else {
                 bail!("Couldn't find the window")
             }
         }
         CommandType::LeaveFrame => {
-            *client = runtime.block_on(client.clone().enter_parent_frame())?;
+            *client = client.clone().enter_parent_frame().await?;
             Ok(elem)
         }
         CommandType::PrintSource => {
-            eprintln!("{}", runtime.block_on(client.source())?);
+            eprintln!("{}", client.source().await?);
             Ok(elem)
         }
         CommandType::Wait => {
             let locator = get_next_locator()?;
-            runtime.block_on(client.wait_for_find(locator))?;
+            client.wait_for_find(locator).await?;
             Ok(elem)
         }
         // Handle command types which need a element.
         _ => {
             let locator = get_next_locator()?;
             let mut new_elem: Element = if let Some(mut e) = elem {
-                runtime.block_on(e.find(locator))?
+                e.find(locator).await?
             } else {
-                runtime.block_on(client.find(locator))?
+                client.find(locator).await?
             };
 
             match &command.command_type {
                 CommandType::Clear => {
-                    runtime.block_on(new_elem.clear())?;
+                    new_elem.clear().await?;
                     Ok(None)
                 }
                 CommandType::EnterFrame => {
-                    *client = runtime.block_on(new_elem.enter_frame())?;
+                    *client = new_elem.enter_frame().await?;
                     Ok(None)
                 }
                 CommandType::ClickUntilNavigation => {
-                    let curr_url = runtime.block_on(client.current_url())?;
+                    let curr_url = client.current_url().await?;
                     loop {
-                        *client = runtime.block_on(new_elem.click())?;
-                        let new_url = runtime.block_on(client.current_url())?;
+                        *client = new_elem.click().await?;
+                        let new_url = client.current_url().await?;
                         if curr_url != new_url {
-                            break Ok(None)
+                            break Ok(None);
                         } else {
-                            new_elem = runtime.block_on(client.find(get_next_locator()?))?;
+                            new_elem = client.find(get_next_locator()?).await?;
                         }
                     }
                 }
                 CommandType::ClickUntilDomChanged => {
-                    let curr_url = runtime.block_on(client.source())?;
+                    let curr_url = client.source().await?;
                     loop {
-                        *client = runtime.block_on(new_elem.click())?;
-                        let new_url = runtime.block_on(client.source())?;
+                        *client = new_elem.click().await?;
+                        let new_url = client.source().await?;
                         if curr_url != new_url {
-                            break Ok(None)
+                            break Ok(None);
                         } else {
-                            new_elem = runtime.block_on(client.find(get_next_locator()?))?;
+                            new_elem = client.find(get_next_locator()?).await?;
                         }
                     }
                 }
                 CommandType::Click => {
-                    *client = runtime.block_on(new_elem.click())?;
+                    *client = new_elem.click().await?;
                     Ok(None)
                 }
                 CommandType::Input(s) => {
-                    runtime.block_on(new_elem.send_keys(s))?;
+                    new_elem.send_keys(s).await?;
                     Ok(None)
                 }
                 CommandType::Recursive(_) => Ok(Some(new_elem)),
